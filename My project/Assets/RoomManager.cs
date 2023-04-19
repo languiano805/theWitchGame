4249508f02ed40816d7e1ccbb4492db72912d468
May, Joseph <josephmay@csus.edu>
2023-04-19 16:20:43 -0700

    room switches based on collision

--- My project/Assets/RoomManager.cs
+++ My project/Assets/RoomManager.cs
@@ -3,41 +3,23 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> rooms;
+    [SerializeField] private GameObject targetRoom;
+    [SerializeField] private GameObject currentRoom;
     [SerializeField] private GameObject player;
 
-    private int currentRoomIndex = 0;
+    private void Start() {
+        
+    }
 
-    private void Start()
-    {
-        // Initialize the rooms by disabling all rooms except the first one
-        for (int i = 1; i < rooms.Count; i++)
-        {
-            rooms[i].SetActive(false);
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")){
+            SwitchRoom();
         }
     }
 
-    private void Update()
+    private void SwitchRoom()
     {
-        // Press the "N" key to switch to the next room
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            SwitchToNextRoom();
-        }
-    }
-
-    private void SwitchToNextRoom()
-    {
-        // Deactivate the current room
-        rooms[currentRoomIndex].SetActive(false);
-
-        // Update the current room index
-        currentRoomIndex = (currentRoomIndex + 1) % rooms.Count;
-
-        // Activate the next room
-        rooms[currentRoomIndex].SetActive(true);
-
-        // Teleport the player to the new room
-        player.transform.position = rooms[currentRoomIndex].transform.position + Vector3.up * 2;
+        currentRoom.SetActive(false);
+        targetRoom.SetActive(true);
     }
 }
